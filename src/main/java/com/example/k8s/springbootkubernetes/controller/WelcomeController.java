84770b41655959887ee94e3e528f6fcaf52cde3a
Felipe Santaniello <you@example.com>
2020-12-30 17:44:40 -0300

    Implemented spring cloud config

--- src/main/java/com/example/k8s/springbootkubernetes/controller/WelcomeController.java
+++ src/main/java/com/example/k8s/springbootkubernetes/controller/WelcomeController.java
@@ -1,14 +1,18 @@
 package com.example.k8s.springbootkubernetes.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+
+	@Value("${test}")
+	private String user;
 	
 	@RequestMapping("/greeting")
 	public String greeting() {
-		return "Welcome to my youtube channel";
+		return "Welcome to my youtube channel " + user;
 	}
 
 }
